3a184b4ad37e119415b12a84d0bc4f3a9479f3b5
M ramdan Ismail <91749019+MRamdan29@users.noreply.github.com>
2022-09-21 17:09:43 +0700

    Add files via upload

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,26 +2,18 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow*
+let str = `*Engga ada SC bang 🎮*
 
-Pengen mentahan nya ?
-Nih ada santai bro....
-https://github.com/ImYanXiao/Elaina-MultiDevice
+Untuk sewa Bot silahkan hubungi Owner 
 
-Mau Yang No Error Script Bot nya, Gas Cek Video YT :
-https://youtu.be/Pgv_GuQTPyo
-
-Pengen yang udah di recode oleh owner?
-Script bot ini dijual bila kalian mau bisa chat owner
-
-wa.me/6282127487538`
+wa.me/6285798048936`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://Instagram.com/ramdan_we",
     mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    description: "https://www.instagram.com/p/Cb91yKIr9K5/?igshid=MTA0ZTI1NzA=", 
+    title: 'FR BOT MOD MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
