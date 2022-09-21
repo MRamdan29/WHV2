612f86877c8e153a6121f1cc455dc847499332bc
M ramdan Ismail <91749019+MRamdan29@users.noreply.github.com>
2022-09-21 17:23:02 +0700

    Update Waalaikumsalam.js

--- plugins/Waalaikumsalam.js
+++ plugins/Waalaikumsalam.js
@@ -47,7 +47,7 @@ conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply
       mediaUrl: "https://github.com/ImYanXiao",
       mediaType: 2,
       description: "https://github.com/ImYanXiao", 
-      title: 'whmods-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
+      title: 'FR BOT MODS-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
       body: wm,
       thumbnail: thumb,
       sourceUrl: sig  }}})
